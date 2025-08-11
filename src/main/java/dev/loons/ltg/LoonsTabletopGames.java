785aac3e6b5516157118136db18bf74fbb7ad57b
loonsthemoons <loonsthemoons@proton.me>
2025-08-11 21:10:04 +0200

    Initial Commit

---
+++ src/main/java/dev/loons/ltg/LoonsTabletopGames.java
@@ -0,0 +1,17 @@
+package dev.loons.ltg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LoonsTabletopGames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
